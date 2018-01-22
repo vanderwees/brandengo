5a3ee37f8508eab459a6963ec56d887e02c4e810
Bernard <bernard@vdwe.es>
2018-01-22 21:07:42 +0100

    Add redux bindings in init file

--- app/js/initialize.js
+++ app/js/initialize.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Redux
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import trainStore from './reducers';
+
 import Onboarding from './components/Onboarding';
 
+// Inject Redux devtools hooks
+const store = createStore(trainStore, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<Onboarding />, document.getElementById('stationContainer'));
+  ReactDOM.render(
+    <Provider store={ store }>
+      <Onboarding />
+    </Provider>,
+    document.getElementById('stationContainer')
+  );
 });
